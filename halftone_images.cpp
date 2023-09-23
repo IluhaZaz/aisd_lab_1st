56685b3b46d444e17fe2022e1a652e4aee726d9a
IluhaZaz <133998226+IluhaZaz@users.noreply.github.com>
2023-09-23 22:22:47 +0300

    Определена часть операторов, добавлены тесты

--- halftone_images.cpp
+++ halftone_images.cpp
@@ -56,8 +56,27 @@ private:
 	int _m;
 	int _n;
 public:
+
+	HalftoneImage(int m, int n, T* data)
+	{
+		if (m <= 0 || n <= 0)
+			throw runtime_error("In HalftoneImage(int m, int n, bool rand) : wrong size");
+		_m = m;
+		_n = n;
+		_matrix = new T * [m];
+		for (int i = 0; i < m; i++)
+		{
+			_matrix[i] = new T[n];
+			for (int j = 0; j < n; j++) {
+				_matrix[i][j] = data[i * m + j];
+			}
+		}
+	}
+
 	HalftoneImage(int m, int n, bool rand)
 	{
+		if (m <= 0 || n <= 0)
+			throw runtime_error("In HalftoneImage(int m, int n, bool rand) : wrong size");
 		_m = m;
 		_n = n;
 		_matrix = new T* [m];
@@ -74,7 +93,9 @@ public:
 			}
 		}
 	}
-
+	
+	
+	
 	friend std::ostream& operator<< (std::ostream& out, const HalftoneImage<T>& h)
 	{
 		for (int i = 0; i < h._m; i++)
@@ -87,17 +108,71 @@ public:
 		}
 			;
 		return out;
-	};
+	}
 
 	T& operator()(int i, int j) const 
 	{
 		return _matrix[i][j];
 	}
-	HalftoneImage operator * (const HalftoneImage& other);
-	HalftoneImage operator * (const T& other);
+	
+	friend HalftoneImage operator * (const HalftoneImage& other)
+	{
+		if (_m != other._m || _n != other._n)
+			throw runtime_error("In operator * : different size");
+		HalftoneImage<T> h(_m, _n, 0);
+		for (int i = 0; i < _m; i++)
+		{
+			for (int j = 0; j < _n; j++)
+			{
+				if (_matrix[i][j] * other._matrix[i][j] > std::numeric_limits<T>::max())
+					h._matrix[i][j] = std::numeric_limits<T>::max();
+				else
+					h._matrix[i][j] = _matrix[i][j] * other._matrix[i][j];
+			}
+		}
+		return h;
+	}
+
+	friend HalftoneImage operator + (const HalftoneImage& other)
+	{
+		if (_m != other._m || _n != other._n)
+			throw runtime_error("In operator * : different size");
+		HalftoneImage<T> h(_m, _n, 0);
+		for (int i = 0; i < _m; i++)
+		{
+			for (int j = 0; j < _n; j++)
+			{
+				if (_matrix(i)(j) + other._matrix(i)(j) > std::numeric_limits<T>::max())
+					h._matrix(i)(j) = std::numeric_limits<T>::max();
+				else
+					h._matrix(i)(j) = _matrix(i)(j) + other._matrix(i)(j);
+			}
+		}
+		return h;
+	}
+
+	friend bool operator==(HalftoneImage<T> a, HalftoneImage<T> b)
+	{
+		if (a._m != b._m || a._n != b._n)
+			return false;
+		for (int i = 0; i < a._m; i++)
+		{
+			for (int j = 0; j < a._n; j++)
+				if (a._matrix[i][j] != b._matrix[i][j])
+					return false;
+		}
+		return true;
+	}
+
+	friend bool operator!=(HalftoneImage<T> a, HalftoneImage<T> b)
+	{
+		return !(a == b);
+	}
+	
 	HalftoneImage operator ! ();
 	float count_fill_factor() {};
-
+	//~HalftoneImage();
 };
+
 
 
--- halftone_images_tests.cpp
+++ halftone_images_tests.cpp
@@ -1,8 +1,28 @@
 #include "halftone_images.cpp"
+#include <gtest/gtest.h>
+#include <stdexcept>
+#include <iostream>
+using namespace std;
 
-int main() {
-	HalftoneImage<char> h(3, 3, false);
-	h(0, 2) = 'a';
-	cout << h;
-		return 0;
+TEST(HalftoneImageTests, OperatorCheck) {
+    HalftoneImage<short> h(3, 3, false);
+    h(0, 2) = 3;
+    EXPECT_EQ(3, h(0, 2));
+
+}
+
+TEST(HalftoneImageTests, ExceptionCheck) {
+    EXPECT_ANY_THROW(HalftoneImage<short> h(3, -3, false));
+    EXPECT_ANY_THROW(HalftoneImage<short> h(-3, 4, true));
+}
+
+TEST(HalftoneImageTests, ConstructorCheck) {
+    HalftoneImage<bool> h1(2, 2, false);
+    h1(0, 0) = 0; h1(0, 1) = 1; h1(1, 0) = 1; h1(1, 1) = 0;
+    bool* data = new bool[4] {0,1,1,0 };
+    HalftoneImage<bool> h2(2, 2, data);
+    cout << h1;
+    cout << h2;
+    EXPECT_TRUE(h1 == h2);
+    EXPECT_FALSE(h1 != h2);
 }
